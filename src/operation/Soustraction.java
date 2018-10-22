c417de26da5bdd8c48381bae9de0a87dda6144c0
gr25 <guilaume.rondot25@orange.fr>
2018-10-22 12:41:07 +0200

    Ajout de la classe Soustraction

---
+++ src/operation/Soustraction.java
@@ -0,0 +1,49 @@
+package operation;
+
+import java.util.Scanner;
+
+public class Soustraction {
+	
+	private int num1;
+	private int num2;
+	
+	
+	public Soustraction(int num1, int num2) {
+		this.num1 = num1;
+		this.num2 = num2;
+	}
+	public int getNum1() {
+		return num1;
+	}
+	public void setNum1(int num1) {
+		this.num1 = num1;
+	}
+	public int getNum2() {
+		return num2;
+	}
+	public void setNum2(int num2) {
+		this.num2 = num2;
+	}
+	
+	public static int value(Soustraction add){
+		return add.getNum1() - add.getNum2();
+	}
+	
+	@Override
+	public String toString() {
+		Soustraction add = new Soustraction(num1, num2);
+		return "Addition : "+ num1 + " - " + num2 + " = " + value(add);
+	}
+	public static void main (String[] args){
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter the first argument : ");
+		int num1 = scan.nextInt();
+		System.out.println("Enter the second argument : ");
+		int num2 = scan.nextInt();
+		Soustraction add = new Soustraction(num1, num2);
+		System.out.println(add.toString());
+	}
+	
+	
+
+}
